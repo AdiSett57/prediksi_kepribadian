4f328dd7115ee4417ba3217a9e85bad5ab45988a
Adi Setiawan <87292736+AdiSett57@users.noreply.github.com>
2023-10-20 08:07:37 +0000

    ganti auth ke apiV1

--- app.py
+++ app.py
@@ -266,8 +266,11 @@ def crawling(nama_pengguna):
     api_key_secret = data_api.API_SECRET
     access_token = data_api.ACCESS_TOKEN
     access_token_secret = data_api.ACCESS_TOKEN_SECRET
-    auth = tweepy.OAuth1UserHandler(api_key, api_key_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    # auth = tweepy.OAuth1UserHandler(api_key, api_key_secret)
+    # auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuth1UserHandler(
+    api_key, api_key_secret, access_token, access_token_secret
+    )
     api = tweepy.API(auth)
     if nama_pengguna:
         limit = 100
